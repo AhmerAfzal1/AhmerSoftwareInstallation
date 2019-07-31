b6f1c5767de78dd3b48bbc7c0903245ccade6eee
Ahmer Afzal <ahmerafzal@yahoo.com>
2019-07-31 11:03:21 +0500

    Category buttons created and assign theirs name

--- Ahmer Silent Software Install Program GUI/MainProgram.Designer.cs
+++ Ahmer Silent Software Install Program GUI/MainProgram.Designer.cs
@@ -29,18 +29,171 @@
         private void InitializeComponent()
         {
             this.panelLeftSide = new System.Windows.Forms.Panel();
+            this.buttonUtilities = new System.Windows.Forms.Button();
+            this.buttonPDF = new System.Windows.Forms.Button();
+            this.buttonMultimedia = new System.Windows.Forms.Button();
+            this.buttonMobile = new System.Windows.Forms.Button();
+            this.buttonMajor = new System.Windows.Forms.Button();
+            this.buttonInternet = new System.Windows.Forms.Button();
+            this.buttonDeveloper = new System.Windows.Forms.Button();
+            this.buttonAutoInstall = new System.Windows.Forms.Button();
+            this.panelLeftSide.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelLeftSide
             // 
             this.panelLeftSide.BackColor = System.Drawing.Color.DarkSalmon;
+            this.panelLeftSide.Controls.Add(this.buttonUtilities);
+            this.panelLeftSide.Controls.Add(this.buttonPDF);
+            this.panelLeftSide.Controls.Add(this.buttonMultimedia);
+            this.panelLeftSide.Controls.Add(this.buttonMobile);
+            this.panelLeftSide.Controls.Add(this.buttonMajor);
+            this.panelLeftSide.Controls.Add(this.buttonInternet);
+            this.panelLeftSide.Controls.Add(this.buttonDeveloper);
+            this.panelLeftSide.Controls.Add(this.buttonAutoInstall);
             this.panelLeftSide.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelLeftSide.Location = new System.Drawing.Point(0, 0);
             this.panelLeftSide.Name = "panelLeftSide";
             this.panelLeftSide.Size = new System.Drawing.Size(200, 812);
             this.panelLeftSide.TabIndex = 0;
             // 
-            // Form1
+            // buttonUtilities
+            // 
+            this.buttonUtilities.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonUtilities.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonUtilities.FlatAppearance.BorderSize = 2;
+            this.buttonUtilities.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGreen;
+            this.buttonUtilities.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonUtilities.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUtilities.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUtilities.ForeColor = System.Drawing.Color.Black;
+            this.buttonUtilities.Location = new System.Drawing.Point(5, 676);
+            this.buttonUtilities.Name = "buttonUtilities";
+            this.buttonUtilities.Size = new System.Drawing.Size(190, 70);
+            this.buttonUtilities.TabIndex = 7;
+            this.buttonUtilities.Text = "button1";
+            this.buttonUtilities.UseVisualStyleBackColor = false;
+            // 
+            // buttonPDF
+            // 
+            this.buttonPDF.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonPDF.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonPDF.FlatAppearance.BorderSize = 2;
+            this.buttonPDF.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGreen;
+            this.buttonPDF.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonPDF.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPDF.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPDF.ForeColor = System.Drawing.Color.Black;
+            this.buttonPDF.Location = new System.Drawing.Point(5, 589);
+            this.buttonPDF.Name = "buttonPDF";
+            this.buttonPDF.Size = new System.Drawing.Size(190, 70);
+            this.buttonPDF.TabIndex = 6;
+            this.buttonPDF.Text = "button1";
+            this.buttonPDF.UseVisualStyleBackColor = false;
+            // 
+            // buttonMultimedia
+            // 
+            this.buttonMultimedia.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonMultimedia.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonMultimedia.FlatAppearance.BorderSize = 2;
+            this.buttonMultimedia.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGreen;
+            this.buttonMultimedia.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonMultimedia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMultimedia.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMultimedia.ForeColor = System.Drawing.Color.Black;
+            this.buttonMultimedia.Location = new System.Drawing.Point(5, 502);
+            this.buttonMultimedia.Name = "buttonMultimedia";
+            this.buttonMultimedia.Size = new System.Drawing.Size(190, 70);
+            this.buttonMultimedia.TabIndex = 5;
+            this.buttonMultimedia.Text = "button1";
+            this.buttonMultimedia.UseVisualStyleBackColor = false;
+            // 
+            // buttonMobile
+            // 
+            this.buttonMobile.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonMobile.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonMobile.FlatAppearance.BorderSize = 2;
+            this.buttonMobile.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGreen;
+            this.buttonMobile.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonMobile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMobile.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMobile.ForeColor = System.Drawing.Color.Black;
+            this.buttonMobile.Location = new System.Drawing.Point(5, 415);
+            this.buttonMobile.Name = "buttonMobile";
+            this.buttonMobile.Size = new System.Drawing.Size(190, 70);
+            this.buttonMobile.TabIndex = 4;
+            this.buttonMobile.Text = "button1";
+            this.buttonMobile.UseVisualStyleBackColor = false;
+            // 
+            // buttonMajor
+            // 
+            this.buttonMajor.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonMajor.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonMajor.FlatAppearance.BorderSize = 2;
+            this.buttonMajor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGreen;
+            this.buttonMajor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonMajor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMajor.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMajor.ForeColor = System.Drawing.Color.Black;
+            this.buttonMajor.Location = new System.Drawing.Point(5, 328);
+            this.buttonMajor.Name = "buttonMajor";
+            this.buttonMajor.Size = new System.Drawing.Size(190, 70);
+            this.buttonMajor.TabIndex = 3;
+            this.buttonMajor.Text = "button1";
+            this.buttonMajor.UseVisualStyleBackColor = false;
+            // 
+            // buttonInternet
+            // 
+            this.buttonInternet.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonInternet.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonInternet.FlatAppearance.BorderSize = 2;
+            this.buttonInternet.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGreen;
+            this.buttonInternet.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonInternet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInternet.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInternet.ForeColor = System.Drawing.Color.Black;
+            this.buttonInternet.Location = new System.Drawing.Point(5, 241);
+            this.buttonInternet.Name = "buttonInternet";
+            this.buttonInternet.Size = new System.Drawing.Size(190, 70);
+            this.buttonInternet.TabIndex = 2;
+            this.buttonInternet.Text = "button1";
+            this.buttonInternet.UseVisualStyleBackColor = false;
+            // 
+            // buttonDeveloper
+            // 
+            this.buttonDeveloper.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonDeveloper.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonDeveloper.FlatAppearance.BorderSize = 2;
+            this.buttonDeveloper.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGreen;
+            this.buttonDeveloper.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonDeveloper.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDeveloper.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDeveloper.ForeColor = System.Drawing.Color.Black;
+            this.buttonDeveloper.Location = new System.Drawing.Point(5, 154);
+            this.buttonDeveloper.Name = "buttonDeveloper";
+            this.buttonDeveloper.Size = new System.Drawing.Size(190, 70);
+            this.buttonDeveloper.TabIndex = 1;
+            this.buttonDeveloper.Text = "button1";
+            this.buttonDeveloper.UseVisualStyleBackColor = false;
+            // 
+            // buttonAutoInstall
+            // 
+            this.buttonAutoInstall.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonAutoInstall.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonAutoInstall.FlatAppearance.BorderSize = 2;
+            this.buttonAutoInstall.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleGreen;
+            this.buttonAutoInstall.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonAutoInstall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAutoInstall.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAutoInstall.ForeColor = System.Drawing.Color.Black;
+            this.buttonAutoInstall.Location = new System.Drawing.Point(5, 67);
+            this.buttonAutoInstall.Name = "buttonAutoInstall";
+            this.buttonAutoInstall.Size = new System.Drawing.Size(190, 70);
+            this.buttonAutoInstall.TabIndex = 0;
+            this.buttonAutoInstall.Text = "button1";
+            this.buttonAutoInstall.UseVisualStyleBackColor = false;
+            // 
+            // MainProgram
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -48,8 +201,11 @@
             this.ClientSize = new System.Drawing.Size(1184, 812);
             this.Controls.Add(this.panelLeftSide);
             this.ForeColor = System.Drawing.Color.Black;
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainProgram";
+            this.Text = "Ahmer\'s Automatically And Silently Softwares Installation Program GUI";
+            this.panelLeftSide.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -57,6 +213,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panelLeftSide;
+        private System.Windows.Forms.Button buttonAutoInstall;
+        private System.Windows.Forms.Button buttonUtilities;
+        private System.Windows.Forms.Button buttonPDF;
+        private System.Windows.Forms.Button buttonMultimedia;
+        private System.Windows.Forms.Button buttonMobile;
+        private System.Windows.Forms.Button buttonMajor;
+        private System.Windows.Forms.Button buttonInternet;
+        private System.Windows.Forms.Button buttonDeveloper;
     }
 }
 
--- Ahmer Silent Software Install Program GUI/MainProgram.cs
+++ Ahmer Silent Software Install Program GUI/MainProgram.cs
@@ -12,9 +12,27 @@ namespace Ahmer_Silent_Software_Install_Program_GUI
 {
     public partial class MainProgram : Form
     {
+        private const string titleHeading = "Ahmer's Automatically And Silently Softwares Installation Program GUI";
+        private const string button1 = "Auto Drivers And Recommended Softwares";
+        private const string button2 = "Developer Softwares";
+        private const string button3 = "Internet Softwares";
+        private const string button4 = "Major Softwares";
+        private const string button5 = "Mobile Softwares";
+        private const string button6 = "Multimedia Softwares";
+        private const string button7 = "PDF Softwares";
+        private const string button8 = "Utilities Softwares";
+
         public MainProgram()
         {
             InitializeComponent();
+            buttonAutoInstall.Text = button1;
+            buttonDeveloper.Text = button2;
+            buttonInternet.Text = button3;
+            buttonMajor.Text = button4;
+            buttonMobile.Text = button5;
+            buttonMultimedia.Text = button6;
+            buttonPDF.Text = button7;
+            buttonUtilities.Text = button8;
         }
     }
 }
