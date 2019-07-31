b4483ca1336a8381210ca721cd43ff4d492a318f
Ahmer Afzal <ahmerafzal@yahoo.com>
2019-07-31 10:27:12 +0500

    Initial commit

---
+++ Ahmer Silent Software Install Program GUI/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ahmer_Silent_Software_Install_Program_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
