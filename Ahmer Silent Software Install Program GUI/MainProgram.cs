7ec9eb1c26f24b9b81cafc90f638e12ab3792e50
Ahmer Afzal <ahmerafzal@yahoo.com>
2019-07-31 15:53:09 +0500

    Almost done but still not tested

--- Ahmer Silent Software Install Program GUI/MainProgram.cs
+++ Ahmer Silent Software Install Program GUI/MainProgram.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ionic.Zip;
 
 namespace Ahmer_Silent_Software_Install_Program_GUI
 {
@@ -22,8 +24,17 @@ namespace Ahmer_Silent_Software_Install_Program_GUI
         private const string titlePDF = "PDF Softwares";
         private const string titleUtilities = "Utilities Softwares";
 
+        private static BackgroundWorker extractFile;
+        private long fileSize;    //the size of the zip file
+        private long extractedSizeTotal;    //the bytes total extracted
+        private long compressedSize;    //the size of a single compressed file
+        private string compressedFileName;    //the name of the file being extracted
+
         private static Label programFileLabel = null;
         private static Label tempFolderLabel = null;
+
+        private static ProgressBar individual = null;
+        private static ProgressBar total = null;
 
         public MainProgram()
         {
@@ -46,12 +57,87 @@ namespace Ahmer_Silent_Software_Install_Program_GUI
             tempFolderLabel = labelShowDestination;
             tempFolderLabel.Text = Constants.TempFolder;
             programFileLabel = labelShowProgramFile;
+
+            individual = progressBarIndividual;
+            total = progressBarTotal;
+
+            progressBarIndividual.Maximum = int.MaxValue;
+            progressBarTotal.Maximum = int.MaxValue;
+
+            extractFile = new BackgroundWorker();
+            extractFile.DoWork += ExtractFile_DoWork;
+            extractFile.ProgressChanged += ExtractFile_ProgressChanged;
+            extractFile.RunWorkerCompleted += ExtractFile_RunWorkerCompleted;
+            extractFile.WorkerReportsProgress = true;
         }
 
-        public static string GetSetShowProgramFile
+        private void ExtractFile_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            get { return programFileLabel.Text; }
-            set { programFileLabel.Text = value; }
+            progressBarIndividual.Value = int.MaxValue;
+            progressBarTotal.Value = int.MaxValue;
+            MessageBox.Show("Done!");
+        }
+
+        private void ExtractFile_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            labelShowExtractingPath.Text = compressedFileName;
+
+            progressBarIndividual.Value = e.ProgressPercentage;
+
+            //calculate the totalPercent
+            long totalPercent = ((long)e.ProgressPercentage * compressedSize + extractedSizeTotal * int.MaxValue) / fileSize;
+            if (totalPercent > int.MaxValue) totalPercent = int.MaxValue;
+            progressBarTotal.Value = (int)totalPercent;
+        }
+
+        private void ExtractFile_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                string fileName = GetSetShowProgramFile;
+                string extractPath = Constants.TempFolder;
+
+                //get the size of the zip file
+                FileInfo fileInfo = new FileInfo(fileName);
+                fileSize = fileInfo.Length;
+                using (ZipFile zipFile = ZipFile.Read(fileName))
+                {
+                    //reset the bytes total extracted to 0
+                    extractedSizeTotal = 0;
+                    int fileAmount = zipFile.Count;
+                    int fileIndex = 0;
+                    zipFile.ExtractProgress += Zip_ExtractProgress;
+                    foreach (ZipEntry ZipEntry in zipFile)
+                    {
+                        fileIndex++;
+                        compressedFileName = "(" + fileIndex.ToString() + "/" + fileAmount + "): " + ZipEntry.FileName;
+                        //get the size of a single compressed file
+                        compressedSize = ZipEntry.CompressedSize;
+                        ZipEntry.Extract(extractPath, Ionic.Zip.ExtractExistingFileAction.OverwriteSilently);
+                        //calculate the bytes total extracted
+                        extractedSizeTotal += compressedSize;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Zip_ExtractProgress(object sender, ExtractProgressEventArgs e)
+        {
+            if (e.TotalBytesToTransfer > 0)
+            {
+                long percent = e.BytesTransferred * int.MaxValue / e.TotalBytesToTransfer;
+                //Console.WriteLine("Indivual: " + percent);
+                extractFile.ReportProgress((int)percent);
+            }
+        }
+
+        public static void ProgressAsync()
+        {
+            extractFile.RunWorkerAsync();
         }
 
         private void ButtonDeveloper_Click(object sender, EventArgs e)
@@ -70,12 +156,6 @@ namespace Ahmer_Silent_Software_Install_Program_GUI
         {
             labelSoftwareCategory.Text = titleMajor;
             this.majorUC1.BringToFront();
-        }
-
-        private void ButtonAhmer_Click(object sender, EventArgs e)
-        {
-            labelSoftwareCategory.Text = "";
-            this.ahmerUC1.BringToFront();
         }
 
         private void ButtonMobile_Click(object sender, EventArgs e)
@@ -100,6 +180,18 @@ namespace Ahmer_Silent_Software_Install_Program_GUI
         {
             labelSoftwareCategory.Text = titleUtilities;
             this.utilitiesUC1.BringToFront();
+        }
+
+        private void ButtonAhmer_Click(object sender, EventArgs e)
+        {
+            labelSoftwareCategory.Text = "";
+            this.ahmerUC1.BringToFront();
+        }
+
+        public static string GetSetShowProgramFile
+        {
+            get { return programFileLabel.Text; }
+            set { programFileLabel.Text = value; }
         }
     }
 }
